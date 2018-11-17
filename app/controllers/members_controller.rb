30d6cffe8afed84aa0a980d95e08d51d6ab922d3
Matt Farmer <mfarmer@d120.org>
2018-11-17 06:17:45 +0000

    added one-way sign-up flow

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,31 @@
 class MembersController < ApplicationController
+    
+    def new
+        @member=Member.new
+    end
+    
+    def create
+        @member=Member.new(member_params)
+        if @member.valid?
+            @member.save
+            session[:member_id]=@member.id
+            redirect_to member_path(@member)
+        else
+            render 'users/new'
+        end
+    end
+    
+    def show
+        @member=Member.find(params[:id].to_i)
+    end
+    
+    
+    
+    
+    
+    private
+    
+    def member_params
+        params.require(:member).permit(:email, :username, :password, :grad_year, :github_username, :status, :role )
+    end
 end
