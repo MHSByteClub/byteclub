6509cd2b02955a6b4dfd419a2a99241d8b3fc8d7
Matt Farmer <mfarmer@d120.org>
2018-12-02 02:28:49 +0000

    adds admin namespace and user index and edit routes and views

---
+++ app/controllers/admin/members_controller.rb
@@ -0,0 +1,43 @@
+class Admin::MembersController < ApplicationController
+    
+    def new
+        @member=Member.new
+    end
+    
+    def create
+        @member=Member.new(member_params)
+        if @member.valid?
+            @member.save
+            session[:member_id]=@member.id
+            redirect_to member_path(@member)
+        else
+            render 'users/new'
+        end
+    end
+    
+    def index
+        @members=Member.all
+    end
+    
+    
+    def show
+        @member=Member.find(params[:id].to_i)
+    end
+    
+    def edit 
+        @member=Member.find(params[:id].to_i)
+    end
+    
+    def update
+        @member=Member.find(params[:id].to_i)
+        @member.update(member_params)
+        redirect_to member_path(@member)
+    end
+    
+    
+    private
+    
+    def member_params
+        params.require(:member).permit(:email, :username, :password, :grad_year, :github_username, :status, :role )
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  resources :members
+  resources :members, only: [:create, :show, :edit, :update]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root "welcome#home"
+    namespace :admin do 
+      resources :members
+    end
+    
+    
     post "/login" => "sessions#create"
     get "/logout" => "sessions#destroy"
 end
