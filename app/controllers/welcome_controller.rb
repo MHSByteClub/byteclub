96f693da829db5162425de52abcd37ce76725970
Matt Farmer <mfarmer@d120.org>
2018-12-06 21:55:06 +0000

    fixed meeting announcement bug on home view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
     def home
-        @next_meeting=Meeting.upcoming
+        @next_meeting=Meeting.announce
         if(logged_in?)
             redirect_to member_path(current_member)
         end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -2,15 +2,15 @@ class Meeting < ApplicationRecord
     
     scope :next_meeting, -> { where("date > ?", Date.today ).order("date asc").first }
     scope :past, -> { where("date < ?", Date.today) }
-    scope :active, -> { where(active: true).first }
+    scope :active_meetings, -> { where(active: true) }
     
     
     
-    def self.upcoming
-        if self.active
-            self.active
-        else
+    def self.announce
+        if self.active_meetings.length==0
             self.next_meeting
+        else
+            self.active_meetings.order('date asc').first
         end
     end
         
