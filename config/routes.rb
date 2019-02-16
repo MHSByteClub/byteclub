8a0f242204c55e003a14c26bb518ae1e4435599f
Matt Farmer <mfarmer@d120.org>
2019-02-16 23:37:21 +0000

    styling the announcements on front page, adding links to show announcements

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,18 @@ Rails.application.routes.draw do
   resources :members do
     resources :projects
   end
+  
+  
   resources :meeting, only: [:show, :index]
-  resources :projects
+  resources :projects, only: [:show, :index]
+  resources :announcements, only: [:show, :index]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root "welcome#home"
     namespace :admin do
       resources :members
       resources :meetings
+      resources :announcements
     end
     
     
